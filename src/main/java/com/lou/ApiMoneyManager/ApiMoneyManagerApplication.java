ff800f0a220053149701abcb05b2bb13d4856d89
Louise <louiisebld@gmail.com>
2022-10-11 21:37:19 +0200

    init

---
+++ src/main/java/com/lou/ApiMoneyManager/ApiMoneyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lou.ApiMoneyManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoneyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMoneyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lou/ApiMoneyManager/ApiMoneyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lou.ApiMoneyManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoneyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
